011f3d5cba015889331670b9b9da25f62d405bd6
sebastian <leehur_sebastian@hotmail.com>
2020-01-19 01:03:34 +0800

    Added STATIC_ROOT

--- dutchquizzy/settings.py
+++ dutchquizzy/settings.py
@@ -119,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# collected static files go here: python manage.py collectstatic
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
